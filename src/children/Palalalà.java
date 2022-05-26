dece23648f8ea3ea7d56149a5cc7ff1bd3adee27
PalatiniStefano <93666848+PalatiniStefano@users.noreply.github.com>
2022-05-26 22:07:08 +0200

    Update Palalalà.java

--- src/children/Palalalà.java
+++ src/children/Palalalà.java
@@ -12,6 +12,7 @@ public class Palalalà extends main.Batterio{
         goRight = true;
     }
 
+    //move "principale"
     @Override
     protected void move() throws Exception {
         int r = (int) (Math.random() * 2 + 1);
@@ -23,7 +24,33 @@ public class Palalalà extends main.Batterio{
                     goDown = false;
                     y = Food.getHeight() - 1;
                 }
-            } else {
+                for(int i = 0; i < 20; i++) {
+                    for (int j = 0; j < 20; i++){
+                        if(Food.isFood(x-i,y-j)){
+                            x-=i;
+                            y-=j;
+                            break;
+                        }
+                        if(Food.isFood(x+i,y-j)){
+                            x+=i;
+                            y-=j;
+                            break;
+                        }
+                        if(Food.isFood(x-i,y+j)){
+                            x-=i;
+                            y+=j;
+                            break;
+                        }
+                        if(Food.isFood(x+i,y+j)){
+                            x+=i;
+                            y+=j;
+                            break;
+                        }
+                        break;
+                    }
+                }
+            }
+            else {
                 y--;
                 if (y < 0) {
                     goDown = true;
@@ -39,7 +66,33 @@ public class Palalalà extends main.Batterio{
                     goRight = false;
                     x = Food.getWidth() - 1;
                 }
-            } else {
+                for(int i = 0; i < 20; i++) {
+                    for (int j = 0; j < 20; i++){
+                        if(Food.isFood(x-i,y-j)){
+                            x-=i;
+                            y-=j;
+                            break;
+                        }
+                        if(Food.isFood(x+i,y-j)){
+                            x+=i;
+                            y-=j;
+                            break;
+                        }
+                        if(Food.isFood(x-i,y+j)){
+                            x-=i;
+                            y+=j;
+                            break;
+                        }
+                        if(Food.isFood(x+i,y+j)){
+                            x+=i;
+                            y+=j;
+                            break;
+                        }
+                        break;
+                    }
+                }
+            }
+            else {
                 x--;
                 if (x < 0) {
                     goRight = true;
@@ -48,6 +101,46 @@ public class Palalalà extends main.Batterio{
             }
         }
     }
+
+    //move alternativa
+//    @Override
+//    protected void move() throws Exception {
+//        int r = (int) (Math.random() * 2 + 1);
+//
+//        if (r == 1){
+//            if (goDown) {
+//                y++;
+//                if (y > Food.getHeight()) {
+//                    goDown = false;
+//                    y = Food.getHeight() - 1;
+//                }
+//            } else {
+//                y--;
+//                if (y < 0) {
+//                    goDown = true;
+//                    y = 0;
+//                }
+//            }
+//        }
+//
+//        else if (r == 2){
+//            if (goRight) {
+//                x++;
+//                if (x > Food.getWidth()) {
+//                    goRight = false;
+//                    x = Food.getWidth() - 1;
+//                }
+//            } else {
+//                x--;
+//                if (x < 0) {
+//                    goRight = true;
+//                    x = 0;
+//                }
+//            }
+//        }
+//    }
+
+
 
     public Batterio clone() throws CloneNotSupportedException {
         Palalalà clone = (Palalalà)super.clone();
